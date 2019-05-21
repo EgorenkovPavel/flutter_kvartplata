3fc031f7a71e77f73145215330157643520bca51
Pavel <epipasha@gmail.com>
2019-05-21 20:18:03 +0300

    Design coldwater page

--- lib/src/pages/cold_water_page.dart
+++ lib/src/pages/cold_water_page.dart
@@ -1,51 +1,87 @@
 import 'package:flutter/material.dart';
 
-class ColdWaterPage extends StatelessWidget{
+class ColdWaterPage extends StatefulWidget {
+  @override
+  _ColdWaterPageState createState() => _ColdWaterPageState();
+}
+
+class _ColdWaterPageState extends State<ColdWaterPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  Widget rowElement(Widget child1, Widget child2){
+    return Row(
+      children: <Widget>[
+        Flexible(
+          child: child1,
+          flex: 1,
+          fit: FlexFit.tight,
+        ),
+        Flexible(
+          child: child2,
+          flex: 1,
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Cold water"),),
-      body: ListView(
-        children: <Widget>[
-          Row(
+      appBar: AppBar(
+        title: Text("Cold water"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
             children: <Widget>[
-              Flexible(child: Text("Value"), flex: 1, fit: FlexFit.tight,),
-              Flexible(child: TextField(keyboardType: TextInputType.number,), flex: 1,)
+              rowElement(
+                  Text("Previous"),
+                  Text("12000")),
+              rowElement(
+                  Text("Value"),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.right,
+                    validator: (value) {
+                      return 'rewe';
+                    },
+                  )
+              ),
+              rowElement(
+                  Text("Delta"),
+                  Text("100")),
+              rowElement(
+                  Text("Tax"),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.right,
+                    validator: (value) {
+                      return 'rewe';
+                    },
+                  )
+              ),
+              rowElement(
+                  Text("Total"),
+                  Text("100")),
+              Row(
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("Save"),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _formKey.currentState.save();
+                        Navigator.of(context).pop();
+                      }
+                    },
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              )
             ],
           ),
-          Row(
-            children: <Widget>[
-              Expanded(child: Text("Previous")),
-              Text("12000"),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(child: Text("Delta")),
-              Text("100"),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Flexible(child: Text("Delta"), flex: 1, fit: FlexFit.tight,),
-              Flexible(child: TextField(keyboardType: TextInputType.number,), flex: 1,)
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(child: Text("Total")),
-              Text("100"),
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              RaisedButton(child: Text("Save"),onPressed: (){
-                Navigator.of(context).pop();
-              },),
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          )
-        ],
+        ),
       ),
     );
   }
